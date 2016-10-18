a95a3601ce9f0cdef77da4b3c05282236f74d0c5
“carlos <galactictourist@gmail.com>
2016-10-18 14:06:20 -0500

    initial commit

---
+++ client/components/employee_list.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Employees } from '../../imports/collections/employees';
+import EmployeeDetail from './employee_detail';
+
+const PER_PAGE = 20;
+
+class EmployeeList extends Component {
+	componentWillMount() {
+		this.page = 1;
+	}
+
+	handleButtonClick () {
+		Meteor.subscribe('employees', PER_PAGE * (this.page + 1));
+		this.page += 1; 
+	}
+
+	render() {
+
+		//props.employees = array of employee objects
+		return (
+				<div>
+					<div className="employee-list">
+						{this.props.employees.map(employee => 
+							<EmployeeDetail key={employee._id} employee={employee} />
+						)}
+					</div>
+
+					<button onClick={this.handleButtonClick.bind(this)}
+					className="btn btn-primary">Load More...
+					</button>
+				</div>
+			);
+	}
+};
+
+export default createContainer(() => {
+
+	//setup sub
+	Meteor.subscribe('employees', PER_PAGE);
+
+	//return object. this is sent to component as props
+	return { employees: Employees.find({}).fetch() };
+
+}, EmployeeList);
